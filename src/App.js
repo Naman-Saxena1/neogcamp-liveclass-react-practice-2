fd28b1ae7b6beaf9b4e0a1e519d6abfd7f27245e
Naman Saxena <90456667+Naman-Saxena1@users.noreply.github.com>
2022-02-20 18:16:23 +0530

    fix : Added loader during loading phase

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  let [loader, setLoader] = useState(false);
   let [data, setData] = useState([]);
 
   async function onClickHandler() {
+    setLoader(true);
     let response = await axios.get("/api/users");
     console.log("Response : ", response.data.users);
     setData(response.data.users);
+    setLoader(false);
   }
 
   return (
     <div className="App">
-      <h1> Data </h1>
+      {loader ? <h1> Data Loading... </h1> : <h1> Data</h1>}
       <button onClick={onClickHandler}> Click to load data from server </button>
       <ol>
         {data.map((item) => (
